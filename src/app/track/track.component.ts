f8a51fea9b5e6003c63c0b323ef47cd1da4a98fa
2b3q <2bb3qq@gmail.com>
2019-08-18 01:16:33 +0300

    upd

--- src/app/track/track.component.ts
+++ src/app/track/track.component.ts
@@ -12,6 +12,8 @@ import {
 import { AxisService } from 'src/shared/axis.service';
 import { SAVED_SNAPSHOTS } from 'src/shared/tracks';
 
+declare var Hls;
+
 @Component({
   selector: 'app-track',
   templateUrl: './track.component.html',
@@ -33,7 +35,9 @@ export class TrackComponent implements OnInit {
   data: SnapshotData[];
   positionInfo: PositionInfo;
   loaded = false;
+  driveMode = true;
   loading = 0;
+  isLoading = false;
   currentLoads: AxisLoads;
 
   constructor(
@@ -47,6 +51,18 @@ export class TrackComponent implements OnInit {
   }
 
   ngOnInit() {
+    // const video = document.getElementById('video');
+    // if (Hls.isSupported()) {
+    //   const hls = new Hls();
+    //   hls.loadSource(
+    //     'https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8'
+    //   );
+    //   hls.attachMedia(video);
+    //   hls.on(Hls.Events.MANIFEST_PARSED, function() {
+    //     video.play();
+    //   });
+    // }
+
     const { id: trackId } = this.route.snapshot.params;
     this.activity = this.mapService.getCurrentTrack(trackId);
     const { snapshotId, startPoint, gpxData } = this.activity;
@@ -85,16 +101,20 @@ export class TrackComponent implements OnInit {
       // update liveloads
       if (lat === 55.449418 && lng === 37.767726) {
         console.log('Load point reached');
+        this.driveMode = false;
         this.loading += 1;
         // wait 10 point before loading
         if (this.loading <= 10) {
+          this.isLoading = true;
           point.snapshot.data = this.axis.getLoadsFromCurrentWeight(
             this.currentLoads
           );
           // total waitiong 210 points
-        } else if (this.loading === 200) {
+        } else if (this.loading >= 200) {
           // set as loaded
           this.loaded = true;
+          this.isLoading = false;
+          this.loading = 0;
         } else {
           // lift a1
           if (this.currentLoads.a1 < this.snapshot.notEmptyLoads.a1) {
@@ -122,37 +142,71 @@ export class TrackComponent implements OnInit {
             this.currentLoads.a5 += this.addRandom(10, 50);
           }
 
-          console.log('this.currentLoads', this.currentLoads);
+          // console.log('this.currentLoads', this.currentLoads);
           point.snapshot.data = this.axis.getLoadsFromCurrentWeight(
             this.currentLoads
           );
         }
-        // } else if (lat !== 55.449418 && lng !== 37.767726 && lat < 55.449418) {
-        //   console.log('empty load from P1 to P2');
-        //   // add dynamic empty loads
-        //   point.snapshot.data = this.axis.getLoadsFromCurrentWeight(
-        //     this.snapshot.emptyLoads
-        //   );
-        // } else if (lat === 51.6298163 && lng === 39.2576188) {
-        //   console.log('UnLoad point reached');
-        //   point.snapshot.data = this.axis.getLoadsFromCurrentWeight(
-        //     this.snapshot.emptyLoads
-        //   );
-        //   // } else if (lat < 51.6298163 && lng < 39.2576188 && lat > 55.449418) {
+      } else if (lat === 51.630542 && lng === 39.25658) {
+        this.driveMode = false;
+        // this.isLoading = true;
+        // <trkpt lat="51.630542" lon="39.256580">
+        console.log('Unload point reached');
+        this.loading += 1;
+        if (this.loading > 5) {
+          console.log('Unloading...');
+          // unloading
+          // unlift a1
+          if (this.currentLoads.a1 > this.snapshot.emptyLoads.a1) {
+            // tslint:disable-next-line:no-unused-expression
+            this.currentLoads.a1 -= this.addRandom(10, 50);
+          }
+          // unlift a2
+          if (this.currentLoads.a2 > this.snapshot.emptyLoads.a2) {
+            // tslint:disable-next-line:no-unused-expression
+            this.currentLoads.a2 -= this.addRandom(10, 50);
+          }
+          // unlift a3
+          if (this.currentLoads.a3 > this.snapshot.emptyLoads.a3) {
+            // tslint:disable-next-line:no-unused-expression
+            this.currentLoads.a3 -= this.addRandom(10, 50);
+          }
+          // unlift a4
+          if (this.currentLoads.a4 > this.snapshot.emptyLoads.a4) {
+            // tslint:disable-next-line:no-unused-expression
+            this.currentLoads.a4 -= this.addRandom(10, 50);
+          }
+          // unlift a5
+          if (this.currentLoads.a5 > this.snapshot.emptyLoads.a5) {
+            // tslint:disable-next-line:no-unused-expression
+            this.currentLoads.a5 -= this.addRandom(10, 50);
+          }
+        } else if (this.loading >= 70) {
+          this.loaded = false;
+          // this.isLoading = false;
+          // this.unloaded = true;
+        }
+
+        // if (this.isLoading) {
+        console.log('this.currentLoads', this.currentLoads);
+        point.snapshot.data = this.axis.getLoadsFromCurrentWeight(
+          this.currentLoads
+        );
+        // }
       } else {
-        console.log('empty loads');
-        // set cargo weight
-        // this.cargoWeight = 0;
+        this.driveMode = true;
       }
 
-      if (this.loaded === true) {
-        point.snapshot.data = this.axis.getLoadsFromCurrentWeight(
-          this.snapshot.notEmptyLoads
-        );
-      } else {
-        point.snapshot.data = this.axis.getLoadsFromCurrentWeight(
-          this.snapshot.emptyLoads
-        );
+      if (this.driveMode) {
+        if (this.loaded === true) {
+          point.snapshot.data = this.axis.getLoadsFromCurrentWeight(
+            this.snapshot.notEmptyLoads
+          );
+        } else {
+          point.snapshot.data = this.axis.getLoadsFromCurrentWeight(
+            this.snapshot.emptyLoads
+          );
+        }
       }
 
       // calculate total weight from current dynamic
